3f3b3b481e38acc12fb710892eed9fc416284d86
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2021-01-26 00:29:43 +0100

    finish appdrawer

--- lib/widgets/app_drawer.dart
+++ lib/widgets/app_drawer.dart
@@ -1,10 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:idyll/widgets/screen_header.dart';
 
 class AppDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Text('foo'),
-    );
+        child: Center(
+      child: Padding(
+        padding: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            InkWell(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ScreenHeader('Favorites'),
+                  FaIcon(
+                    FontAwesomeIcons.bookmark,
+                  ),
+                ],
+              ),
+              onTap: () {},
+            ),
+            SizedBox(height: 30),
+            InkWell(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ScreenHeader('News'),
+                  FaIcon(FontAwesomeIcons.newspaper),
+                ],
+              ),
+              onTap: () {},
+            ),
+            SizedBox(height: 30),
+            InkWell(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ScreenHeader('Product Hunt'),
+                  FaIcon(FontAwesomeIcons.productHunt),
+                ],
+              ),
+              onTap: () {},
+            ),
+            SizedBox(height: 30),
+            InkWell(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ScreenHeader('Youtube'),
+                  FaIcon(FontAwesomeIcons.youtube),
+                ],
+              ),
+              onTap: () {},
+            ),
+            SizedBox(height: 30),
+            InkWell(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ScreenHeader('Hacker News'),
+                  FaIcon(FontAwesomeIcons.hackerNewsSquare),
+                ],
+              ),
+              onTap: () {},
+            ),
+            SizedBox(height: 30),
+            InkWell(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ScreenHeader('GitHub'),
+                  FaIcon(FontAwesomeIcons.github),
+                ],
+              ),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+    ));
   }
 }
