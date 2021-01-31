5ecc25b1526c77750c865b1cd7a6e37680c1520e
Zubeir Mohamed <zubeir.mohamed@outlook.de>
2021-01-31 22:50:09 +0100

    add crashlytics and analytics for android

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ import 'screens/query_screen.dart';
 import 'screens/privacy_policy_screen.dart';
 import 'providers/query.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
   runApp(
@@ -38,73 +39,91 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Idyll',
-      theme: ThemeData(
-        accentColor: Colors.black,
-      ),
-      home: HomeScreen(),
-      routes: {
-        FavoriteScreen.routeName: (ctx) => FavoriteScreen(),
-        HomeScreen.routeName: (ctx) => HomeScreen(),
-        ProductHuntScreen.routeName: (ctx) => ProductHuntScreen(),
-        YoutubeScreen.routeName: (ctx) => YoutubeScreen(),
-        RedditScreen.routeName: (ctx) => RedditScreen(),
-        HackerNewsScreen.routeName: (ctx) => HackerNewsScreen(),
-        SearchScreen.routeName: (ctx) => SearchScreen(),
-        QueryScreen.routeName: (ctx) => QueryScreen(),
-        PrivacyPolicyScreen.routeName: (ctx) => PrivacyPolicyScreen()
+    return FutureBuilder(
+      // Initialize FlutterFire
+      future: Firebase.initializeApp(),
+      builder: (context, snapshot) {
+        // Check for errors
+
+        // if (snapshot.hasError) {
+        //   return SomethingWentWrong();
+        // }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Idyll',
+            theme: ThemeData(
+              accentColor: Colors.black,
+            ),
+            home: HomeScreen(),
+            routes: {
+              FavoriteScreen.routeName: (ctx) => FavoriteScreen(),
+              HomeScreen.routeName: (ctx) => HomeScreen(),
+              ProductHuntScreen.routeName: (ctx) => ProductHuntScreen(),
+              YoutubeScreen.routeName: (ctx) => YoutubeScreen(),
+              RedditScreen.routeName: (ctx) => RedditScreen(),
+              HackerNewsScreen.routeName: (ctx) => HackerNewsScreen(),
+              SearchScreen.routeName: (ctx) => SearchScreen(),
+              QueryScreen.routeName: (ctx) => QueryScreen(),
+              PrivacyPolicyScreen.routeName: (ctx) => PrivacyPolicyScreen()
+            },
+            localizationsDelegates: [
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+              GlobalCupertinoLocalizations.delegate,
+            ],
+            supportedLocales: [
+              const Locale('de', 'DK'),
+              const Locale('de', 'AT'),
+              const Locale('nl', 'BE'),
+              const Locale('de', 'CH'),
+              const Locale('de', 'DE'),
+              const Locale('en', 'AU'),
+              const Locale('en', 'CA'),
+              const Locale('en', 'GB'),
+              const Locale('en', 'ID'),
+              const Locale('en', 'IE'),
+              const Locale('en', 'IN'),
+              const Locale('en', 'MY'),
+              const Locale('en', 'NZ'),
+              const Locale('en', 'PH'),
+              const Locale('en', 'SG'),
+              const Locale('en', 'US'),
+              const Locale('en', 'WW'),
+              const Locale('en', 'XA'),
+              const Locale('en', 'ZA'),
+              const Locale('es', 'AR'),
+              const Locale('es', 'CL'),
+              const Locale('es', 'ES'),
+              const Locale('es', 'MX'),
+              const Locale('es', 'US'),
+              const Locale('es', 'XL'),
+              const Locale('fi', 'FI'),
+              const Locale('fr', 'BE'),
+              const Locale('fr', 'CA'),
+              const Locale('fr', 'CH'),
+              const Locale('fr', 'FR'),
+              const Locale('it', 'IT'),
+              const Locale('zh', 'HK'),
+              const Locale('zh', 'TW'),
+              const Locale('ja', 'JP'),
+              const Locale('ko', 'KR'),
+              const Locale('nl', 'NL'),
+              const Locale('zh', 'CN'),
+              const Locale('pl', 'PL'),
+              const Locale('pt', 'BR'),
+              const Locale('ru', 'RU'),
+              const Locale('sv', 'SE'),
+              const Locale('tr', 'TR'),
+            ],
+          );
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return CircularProgressIndicator();
       },
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: [
-        const Locale('de', 'DK'),
-        const Locale('de', 'AT'),
-        const Locale('nl', 'BE'),
-        const Locale('de', 'CH'),
-        const Locale('de', 'DE'),
-        const Locale('en', 'AU'),
-        const Locale('en', 'CA'),
-        const Locale('en', 'GB'),
-        const Locale('en', 'ID'),
-        const Locale('en', 'IE'),
-        const Locale('en', 'IN'),
-        const Locale('en', 'MY'),
-        const Locale('en', 'NZ'),
-        const Locale('en', 'PH'),
-        const Locale('en', 'SG'),
-        const Locale('en', 'US'),
-        const Locale('en', 'WW'),
-        const Locale('en', 'XA'),
-        const Locale('en', 'ZA'),
-        const Locale('es', 'AR'),
-        const Locale('es', 'CL'),
-        const Locale('es', 'ES'),
-        const Locale('es', 'MX'),
-        const Locale('es', 'US'),
-        const Locale('es', 'XL'),
-        const Locale('fi', 'FI'),
-        const Locale('fr', 'BE'),
-        const Locale('fr', 'CA'),
-        const Locale('fr', 'CH'),
-        const Locale('fr', 'FR'),
-        const Locale('it', 'IT'),
-        const Locale('zh', 'HK'),
-        const Locale('zh', 'TW'),
-        const Locale('ja', 'JP'),
-        const Locale('ko', 'KR'),
-        const Locale('nl', 'NL'),
-        const Locale('zh', 'CN'),
-        const Locale('pl', 'PL'),
-        const Locale('pt', 'BR'),
-        const Locale('ru', 'RU'),
-        const Locale('sv', 'SE'),
-        const Locale('tr', 'TR'),
-      ],
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:idyll/providers/news.dart';
